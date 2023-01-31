9147d0843ccb8aa6cd8be7fbd0082dce65b2a4c7
Bryce Klinker <bryce.klinker@gmail.com>
2023-01-30 21:53:44 -0600

    adding initial database migrations

--- identity/Klinker.Home.Identity.Web/Common/ServiceCollectionExtensions.cs
+++ identity/Klinker.Home.Identity.Web/Common/ServiceCollectionExtensions.cs
@@ -9,10 +9,13 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddKlinkerHomeIdentityWeb(this IServiceCollection services, IConfiguration config)
     {
         var connectionString = config.GetConnectionString("Identity");
+        if (string.IsNullOrEmpty(connectionString))
+            throw new InvalidOperationException("Missing 'Identity' connection string");
+
         services.AddRazorPages();
         services.AddDbContext<KlinkerIdentityDbContext>(opts =>
         {
-            opts.UseNpgsql(connectionString);
+            opts.UseDatabase(connectionString);
         });
 
         services.AddIdentity<KlinkerUser, KlinkerRole>().AddEntityFrameworkStores<KlinkerIdentityDbContext>();
@@ -21,13 +24,24 @@ public static class ServiceCollectionExtensions
             .AddIdentityServer()
             .AddConfigurationStore(opts =>
             {
-                opts.ConfigureDbContext = o => o.UseNpgsql(connectionString);
+                opts.ConfigureDbContext = o => o.UseDatabase(connectionString);
             })
             .AddOperationalStore(opts =>
             {
-                opts.ConfigureDbContext = o => o.UseNpgsql(connectionString);
+                opts.ConfigureDbContext = o => o.UseDatabase(connectionString);
             })
             .AddAspNetIdentity<KlinkerUser>();
         return services;
+    }
+
+    private static void UseDatabase(this DbContextOptionsBuilder builder, string connectionString)
+    {
+        builder.UseNpgsql(
+            connectionString,
+            o =>
+            {
+                o.MigrationsAssembly(typeof(ServiceCollectionExtensions).Assembly.GetName().Name);
+            }
+        );
     }
 }
